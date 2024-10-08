1ce24200068362c9612d57c1138767ab6b59db2b
JS-Marinn <mrsebastian097@gmail.com>
2024-10-07 22:06:43 -0500

    "Se conectó el back y el front"

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,22 @@
 import express from "express";
+import cors from 'cors';
 import dotenv from 'dotenv';
 import conectarDB from "./config/db.js";
 import usuarioRoutes from './routes/usuarioRoutes.js';
 import proveedorRoutes from './routes/proveedorRoutes.js';
-import pedidoRoutes from './routes/pedidoRoutes.js'; 
-//import proyectoRoutes from './routes/proyectoRoutes';
+import pedidoRoutes from './routes/pedidoRoutes.js';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
-
 dotenv.config();
-
 conectarDB();
 
-//Configurar CORS
-
-// // Routing
 app.use("/api/usuarios", usuarioRoutes);
 app.use("/api/proveedores", proveedorRoutes); 
-app.use("/api/pedidos", pedidoRoutes); 
-//app.use('/api/proyectos', proyectoRoutes);
+app.use("/api/pedidos", pedidoRoutes);
 
 const PORT = process.env.PORT || 4000;
-
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
