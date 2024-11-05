d31326c5d444080118704071779a40254a758462
JS-Marinn <mrsebastian097@gmail.com>
2024-11-05 18:50:28 -0500

    "Estilos css en general"

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 
 const NavBar = () => {
-  const { user, logout } = useContext(AuthContext); // Acceder al usuario y la función logout desde el contexto
-  const userName = user ? user.nombre : ''; // Nombre del usuario
-  const userRole = user ? user.role : ''; // Rol del usuario
+  const { user, logout } = useContext(AuthContext);
+  const userName = user ? user.nombre : '';
+  const userRole = user ? user.role : '';
 
   return (
     <div className="navbar">
@@ -14,14 +14,29 @@ const NavBar = () => {
         <img className="Logo" src={Logo} alt="logo" />
       </div>
 
-      <div className="navbar-section links">
-        <Link to="/pharmacy">Drogueria</Link>
-        <Link to="/home">Hogar</Link>
-        <Link to="/"><i className="home icon"></i></Link>
-        <Link to="/market">Mercado</Link>
-        <Link to="/tek">Tecnologia</Link>
-        {/* Mostrar la opción de Proveedores solo si el rol es admin */}
-        {userRole === 'admin' && <Link to="/proveedores">Proveedores</Link>}
+      <div className={`navbar-section links ${userRole === 'admin' ? 'admin-links' : 'user-links'}`}>
+        {/* Si el usuario es admin, mostrar 7 enlaces; si no, mostrar solo 5 */}
+        {userRole === 'admin' ? (
+          <>
+            <Link to="/pharmacy">Drogueria</Link>
+            <Link to="/home">Hogar</Link>
+            <Link to="/market">Mercado</Link>
+            {/* Icono de inicio centrado */}
+            <Link to="/" className="centered-home-icon"><i className="home icon"></i></Link>
+            <Link to="/tek">Tecnologia</Link>
+            <Link to="/proveedores">Proveedores</Link>
+            <Link to="/log">Log</Link>
+          </>
+        ) : (
+          <>
+            <Link to="/pharmacy">Drogueria</Link>
+            <Link to="/home">Hogar</Link>
+            {/* Icono de inicio centrado */}
+            <Link to="/" className="centered-home-icon"><i className="home icon"></i></Link>
+            <Link to="/market">Mercado</Link>
+            <Link to="/tek">Tecnologia</Link>
+          </>
+        )}
       </div>
 
       <div className="navbar-section">
@@ -31,9 +46,7 @@ const NavBar = () => {
               <span>¡Hola, {userName}!</span>
             </div>
             <div className="ui vertical animated button" tabIndex="0" onClick={logout} style={{ cursor: 'pointer' }}>
-              <div className="hidden content">
-                Cerrar sesión
-              </div>
+              <div className="hidden content">Cerrar sesión</div>
               <div className="visible content">
                 <i className="log out icon"></i>
               </div>
